99039b8b3ab099cb45bd4fbafc00cc02bb7b273d
AnuragMaurya76 <anuragmaurya9345@gmail.com>
2023-09-07 00:02:21 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { render } from "react-dom";
+import Example from "./DragAndDrop/Example";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = () => {
+   return (
+      <div className="App">
+         <DndProvider backend={HTML5Backend}>
+            <Example />
+         </DndProvider>
+      </div>
+   );
+};
 
-export default App;
+const rootElement = document.getElementById("root");
+render(<App />, rootElement);
